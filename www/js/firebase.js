f6dad34110f862b231a535a6a65c436491cc53dd
Yukios7 <51745445+Yukios7@users.noreply.github.com>
2021-10-05 09:00:30 -0300

    v3
    implementei a função excluir, por favor analise =)

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+	apiKey: "AIzaSyC7GdzpzryFYOtgJIfKYrDyINzo8SiMNbw",
+    authDomain: "androidpam-62a8c.firebaseapp.com",
+    projectId: "androidpam-62a8c",
+    storageBucket: "androidpam-62a8c.appspot.com",
+    messagingSenderId: "517447229875",
+    appId: "1:517447229875:web:062044390a8ffdadecabc6",
+    measurementId: "G-RRC04D7YKP"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
